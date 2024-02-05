9a01f74a5a2d1c40f94d8bfd9f7079d23e4cebc7
Tyrone <tyronejoel04@gmail.com>
2024-02-05 02:10:27 +0200

    first commit

---
+++ src/assets/images/index.js
@@ -0,0 +1,60 @@
+import bigShoe1 from './big-shoe1.png'
+import bigShoe2 from './big-shoe2.png'
+import bigShoe3 from './big-shoe3.png'
+
+import Air1 from './Air-1.jpeg'
+import Air2 from './Air-2.jpeg'
+import Air3 from './Air-3.jpeg'
+import Air4 from './Air-4.jpeg'
+import Air5 from './Air-5.jpeg'
+
+import customer1 from './customer1.jpeg'
+import customer2 from './customer2.svg'
+import tye1 from './tye1.png'
+
+import footerLogo from './footer-logo.svg'
+import headerLogo from './header-logo.svg'
+
+import offer from './offer.svg'
+
+import shoe4 from './shoe4.svg'
+import shoe5 from './shoe5.svg'
+import shoe6 from './shoe6.svg'
+import shoe7 from './shoe7.svg'
+import shoe8 from './shoe8.svg'
+
+import thumbnailBackground from './thumbnail-background.svg'
+import thumbnailShoe1 from './thumbnail-shoe1.svg'
+import thumbnailShoe2 from './thumbnail-shoe2.svg'
+import thumbnailShoe3 from './thumbnail-shoe3.svg'
+
+export {
+    bigShoe1,
+    bigShoe2,
+    bigShoe3,
+
+    Air1,
+    Air2,
+    Air3,
+    Air4,
+    Air5,
+
+    customer1,
+    customer2,
+    tye1,
+
+    footerLogo,
+    headerLogo,
+
+    offer,
+    shoe4,
+    shoe5,
+    shoe6,
+    shoe7,
+    shoe8,
+
+    thumbnailBackground,
+    thumbnailShoe1,
+    thumbnailShoe2,
+    thumbnailShoe3
+}
